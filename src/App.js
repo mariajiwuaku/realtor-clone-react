a4d6a2bd9383dcb9803a6cb92b8ce66a3fae2075
Maria-goretti Jiwuaku <mariagorettiduruigbo@gmail.com>
2023-08-01 15:00:02 -0700

    pages created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import Profile from "./pages/Profile";
+import Offers from "./pages/offers";
+import SignUp from "./pages/SignUp";
+import ForgotPassword from "./pages/forgotPassword";
 
 function App() {
   return (
     <div>
-      <h1 className='text-2xl bg-red-500'>Hello World</h1>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<Profile/>}/>
+          <Route path="/signIn" element={<SignIn/>}/>
+          <Route path="/signUp" element={<SignUp/>}/>
+          <Route path="/offers" element={<Offers/>}/>
+          <Route path="/forgotPassword" element={<ForgotPassword/>}/>
+
+
+          
+
+          
+
+        </Routes>
+      </Router>
     </div>
   );
 }
